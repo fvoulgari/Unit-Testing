7dc15b8f513322061d26bf987b0b1d4ae992524b
fvoulgari <fayrevof@gmail.com>
2022-04-22 16:33:16 +0300

    test

--- test_multiply.py
+++ test_multiply.py
@@ -5,7 +5,9 @@ from multiply import multiplication
 class TestMultiply(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(multiplication(2,3),6)
-        self.assertEqual(multiplication(5,10),50)
+        self.assertEqual(multiplication(-1,-1),1 )
+        self.assertEqual(multiplication(-1,1),-1)
+
 
 
 if __name__ == '__main__':
